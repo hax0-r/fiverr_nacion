17374a76728235a38746fcd9d04e335210eab155
Muhammad Talha <talha18513@gmail.com>
2025-03-22 18:40:10 +0500

    first

--- Docs/javascript/global.js
+++ Docs/javascript/global.js
@@ -9,40 +9,25 @@ requestAnimationFrame(raf);
 
 // scroll to the top
 
-const bottomToTopScroll = document.getElementById("bottomToTopScroll");
+// const bottomToTopScroll = document.getElementById("bottomToTopScroll");
 
-bottomToTopScroll.innerHTML = `
-<div
-    class="bottomToTop fadeIn w-10 cursor-pointer hidden h-10 fixed bottom-5 right-5 flex items-center justify-center rounded-lg border-2 border-[#262626]"><i class="fa-solid fa-arrow-up"></i>
-</div>`
+// bottomToTopScroll.innerHTML = `
+// <div
+//     class="bottomToTop fadeIn w-10 cursor-pointer hidden h-10 fixed bottom-5 right-5 flex items-center justify-center rounded-lg border-2 border-[#262626]"><i class="fa-solid fa-arrow-up"></i>
+// </div>`
 
-document.addEventListener("DOMContentLoaded", function () {
-    const scrollToTopBtn = document.querySelector(".bottomToTop");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const scrollToTopBtn = document.querySelector(".bottomToTop");
 
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 200) {
-            scrollToTopBtn.style.display = "flex";
-        } else {
-            scrollToTopBtn.style.display = "none";
-        }
-    });
+//     window.addEventListener("scroll", function () {
+//         if (window.scrollY > 200) {
+//             scrollToTopBtn.style.display = "flex";
+//         } else {
+//             scrollToTopBtn.style.display = "none";
+//         }
+//     });
 
-    scrollToTopBtn.addEventListener("click", function () {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    });
-});
-
-// form 
-
-const fileInput = document.getElementById('file-upload');
-const uploadBtn = document.getElementById('upload-btn');
-const uploadArea = document.getElementById('upload-area');
-
-uploadBtn.addEventListener('click', function (event) {
-    event.stopPropagation();
-    fileInput.click();
-});
-
-uploadArea.addEventListener('click', function () {
-    fileInput.click();
-});
+//     scrollToTopBtn.addEventListener("click", function () {
+//         window.scrollTo({ top: 0, behavior: "smooth" });
+//     });
+// });
